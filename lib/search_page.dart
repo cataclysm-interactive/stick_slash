6c0b5df8e3d9aa96c55b1d892fd12a9499a3cafa
UntoldTitan <untoldtitan3k@gmail.com>
2022-11-03 15:27:26 -0600

    New Search Page

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -15,27 +15,16 @@ class _AllCardsPageState extends State<AllCardsPage> {
   bool error = false; //for error status
   bool loading = false; //for data featching status
   String errmsg = ""; //to assing any error message from API/runtime
+  String errCode = "";
   // ignore: prefer_typing_uninitialized_variables
   var apidata; //for decoded JSON data
   // It's JSON data from my API, and I don't feel like writing a type for it
 
-  List cards2015 = [];
-  List cards2016 = [];
-  List cards2017 = [];
-  List cards2018 = [];
-  List cards2019 = [];
-  List cards2020 = [];
-  List cards2021 = [];
-  List cardsCanada = [];
-  List cards2022 = [];
-
-  List<List> cardArrays = [];
+  Map<String, List> cards = {};
 
   List<String> setNames = [];
 
   String setFilterValue = "All";
-
-  List<List> cardsFinal = [];
 
   @override
   void initState() {
@@ -44,103 +33,34 @@ class _AllCardsPageState extends State<AllCardsPage> {
   }
 
   getData() async {
+    //TODO: Remake the sorting algorythmn to actually sort the cards properly, and nicely
     setNames.add("All");
-    setState(() {
-      loading = true; //make loading true to show progressindicator
-    });
+    loading = true; //make loading true to show progressindicator
+    setState(() {});
 
-    String url = "https://timhortonsapi.azurewebsites.net/api/hockeycards";
-    //don't use "http://localhost/" use local IP or actual live URL
+    String baseUrl = "https://timhortonsapi.azurewebsites.net/api/";
 
-    Response response = await dio.get(url);
-    apidata = response.data; //get JSON decoded data from response
+    Response cardsRes = await dio.get('${baseUrl}hockeycards');
+    var cardData = cardsRes.data;
 
-    if (response.statusCode != 200) {
+    if (cardsRes.statusCode != 200) {
       error = true;
-      errmsg = "Error while fetching data.";
-      return;
+      errmsg = cardsRes.data;
+      errCode = cardsRes.statusCode.toString();
     }
 
-    // Yes, yes, I know. Switch case statements exist, and I should probably
-    // use one here, but idc, and it was giving weird errors.
-    apidata.forEach(
-      (element) {
-        if (element["year"] == "2015") {
-          cards2015.add(element);
-        } else if (element["year"] == "2016") {
-          cards2016.add(element);
-        } else if (element["year"] == "2017") {
-          cards2017.add(element);
-        } else if (element["year"] == "2018") {
-          cards2018.add(element);
-        } else if (element["year"] == "2019") {
-          cards2019.add(element);
-        } else if (element["year"] == "2020") {
-          cards2020.add(element);
-        } else if (element["year"] == "2021") {
-          cards2021.add(element);
-        } else if (element["year"] == "2022") {
-          cards2022.add(element);
-        } else if (element["year"] == "2021-can") {
-          cardsCanada.add(element);
-        }
-        bool matching = false;
-        for (var setName in setNames) {
-          if (element["set"] == setName) {
-            matching = true;
-          }
-        }
-        if (matching == false) {
-          setNames.add(element["set"]);
-        }
-      },
-    );
-
-    // TODO: Add the rest of the cards from other sets into the API
-    loading = false;
-
-    resetCards();
-
-    //This is because cardArrays will be modified and changed as time goes on.
-    setState(() {}); //refresh UI
-  }
-
-  resetCards() {
-    print("Resetting Cards");
-    //MUST BE IN THE SAME ORDER AS THE TABS IN THE APP BAR
-    cardArrays = [];
-    print(cards2015.length);
-    cardArrays.add(cards2015);
-    cardArrays.add(cards2016);
-    cardArrays.add(cards2017);
-    cardArrays.add(cards2018);
-    cardArrays.add(cards2019);
-    cardArrays.add(cards2020);
-    cardArrays.add(cards2021);
-    cardArrays.add(cardsCanada);
-    cardArrays.add(cards2022);
-  }
-
-  filterCards() {
-    //Sets
-    resetCards();
-    print("Filtering");
-    print(cardArrays.length);
-    print(cardArrays[0].length);
-    if (setFilterValue != "All") {
-      for (var year in cardArrays) {
-        List cardsToRemove = [];
-        for (var card in year) {
-          if (card["set"] != setFilterValue) {
-            cardsToRemove.add(card);
-          }
-        }
-        for (var card in cardsToRemove) {
-          year.remove(card);
-        }
+    for (var card in cardData) {
+      if (cards[card["year"]] == null) {
+        cards[card["year"]] = [];
+        cards[card["year"]]!.add(card);
+      } else {
+        cards[card["year"]]!.add(card);
       }
     }
-    setState(() {});
+
+    loading = false;
+
+    setState(() {}); //refresh UI
   }
 
   @override
@@ -151,72 +71,61 @@ class _AllCardsPageState extends State<AllCardsPage> {
         appBar: AppBar(
           title: const Text("All Cards"),
           foregroundColor: Colors.white,
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: "2015",
-              ),
-              Tab(
-                text: "2016",
-              ),
-              Tab(
-                text: "2017",
-              ),
-              Tab(
-                text: "2018",
-              ),
-              Tab(
-                text: "2019",
-              ),
-              Tab(
-                text: "2020",
-              ),
-              Tab(
-                text: "2021",
-              ),
-              Tab(
-                text: "2021 Team Canada",
-              ),
-              Tab(
-                text: "2022",
-              ),
-            ],
-            isScrollable: true,
-          ),
+          bottom: loading
+              ? null
+              : TabBar(
+                  tabs: cards.keys
+                      .map<Widget>(
+                        (e) => Tab(
+                          text: e,
+                        ),
+                      )
+                      .toList(),
+                  isScrollable: true,
+                ),
           backgroundColor: Colors.red[600],
         ),
         body: loading
-            ? const Center(child: CircularProgressIndicator())
-            : Center(
-                child: error
-                    ? Text("Error: $errmsg")
-                    : TabBarView(
-                        children: cardArrays
-                            .map(
-                              (cardArray) => ListView(
-                                children: cardArray
-                                    .map(
-                                      (card) => ListTile(
-                                        title: Text(
-                                          card["playerName"],
-                                        ),
-                                        subtitle: Text(
-                                          card["set"],
-                                        ),
-                                        trailing: Text(
-                                          "\$${card["price"].toString()}",
-                                        ),
-                                        onTap: () {
-                                          //TODO: Display information about the card
-                                        },
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            )
-                            .toList(),
-                      ),
-              ),
+            ? Center(
+                child: CircularProgressIndicator(
+                color: Colors.red[600],
+              ))
+            : error
+                ? Column(
+                    children: [
+                      const Text("An Error Occured"),
+                      Text(errCode),
+                      Text(errmsg),
+                      const Text("Please try again later")
+                    ],
+                  )
+                : Center(
+                    child: error
+                        ? Text("Error: $errmsg")
+                        : TabBarView(
+                            children: cards.values
+                                .map(
+                                  (yearOfCards) => ListView(
+                                    children: yearOfCards
+                                        .map(
+                                          (card) => ListTile(
+                                            title: Text(card["playerName"]),
+                                            trailing: Text(
+                                                "\$${card["price"].toString()}"),
+                                            subtitle: Text(card["set"] +
+                                                "   " +
+                                                card["year"]),
+                                            onTap: () {
+                                              print("Tapped");
+                                            },
+                                          ),
+                                        )
+                                        .toList(),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                  ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             showBottomSheet(
@@ -246,7 +155,7 @@ class _AllCardsPageState extends State<AllCardsPage> {
                                 setFilterValue = val!;
                                 setState(() {});
                                 print(setFilterValue);
-                                filterCards();
+                                // filterCards();
                               },
                             ),
                             const Text("Bottom Sheet"),
